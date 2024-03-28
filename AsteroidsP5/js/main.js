7c6612d39576a98145efffdab048385f85f8368c
Oliver <129097817+gingeapple182@users.noreply.github.com>
2024-03-28 19:46:15 +0000

    Created ship

--- AsteroidsP5/js/main.js
+++ AsteroidsP5/js/main.js
@@ -1,7 +1,16 @@
+var ship;
+
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    ship = new Ship();
 }
 
 function draw() {
     background(0);
+    ship.render();
+    ship.turn();
+    ship.update();
+    ship.edges();
 }
+
---
+++ AsteroidsP5/js/ship.js
@@ -0,0 +1,63 @@
+function Ship(){
+    this.pos = createVector(width/2, height/2);
+    this.r = 20;
+    this.heading = 0;
+    this.rotation = 0;
+    this.vel = createVector(0, 0);
+    this.isBoosting = false;
+    this.boosting = function(b) {
+        this.isBoosting = b;
+    }
+
+    this.render = function(){
+        translate(this.pos.x, this.pos.y);
+        rotate(this.heading + PI/2)
+        noFill();
+        stroke(255);
+        triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+    }
+    this.edges = function() {
+        if (this.pos.x > width + this.r) {
+            this.pos.x = -this.r;
+        } else if (this.pos.x < -this.r) {
+            this.pos.x = width + this.r;
+        } else if (this.pos.y > height + this.r) {
+            this.pos.y = -this.r;
+        } else if (this.pos.y < -this.r) {
+            this.pos.y = height + this.r;
+        }
+    }
+    this.setRotation = function(a) {
+        this.rotation = a;
+    }
+    this.turn = function(angle) {
+        this.heading += this.rotation;
+    }
+    this.update = function() {
+        if(this.isBoosting) {
+            this.boost();
+        }
+        this.pos.add(this.vel);
+        this.vel.mult(0.95);
+    }
+    this.boost = function() {
+        var force = p5.Vector.fromAngle(this.heading);
+        force.mult(0.99);
+        this.vel.add(force);
+    }
+}
+
+function keyPressed() {
+    if (keyCode == RIGHT_ARROW) {
+        ship.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW) {
+        ship.setRotation(-0.1);
+    } else if (keyCode == UP_ARROW) {
+        ship.boosting(true);
+    }
+}
+
+function keyReleased() {
+    ship.setRotation(0);
+    ship.boosting(false);
+}
