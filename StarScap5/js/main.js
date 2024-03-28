86b5ccf235b65ed7a223ced48f5ad04cf0feeb9d
Oliver <129097817+gingeapple182@users.noreply.github.com>
2024-03-28 02:41:59 +0000

    StarScap5 project
    Initial code thanks to The Coding Train on youtube

---
+++ StarScap5/js/main.js
@@ -0,0 +1,20 @@
+var stars = [];
+
+var speed;
+
+function setup() {
+  createCanvas(600, 600);
+  for (var i = 0; i < 1000; i++) {
+    stars[i] = new Star();
+  }
+}
+
+function draw() {
+  speed = map(mouseX, 0, width, 10, 20);
+  background(0);
+  translate(width / 2, height / 2);
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
